1434620a18204ca62754879a78c6c866bbc8e9a7
Delyan Dimitrov <delyandimitrov99@gmail.com>
2020-12-05 23:14:33 +0200

    Initial commit

---
+++ AdventOfCodeDay1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCodeDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var list = new List<int>();
+            while ( input != "STOP")
+            {
+                list.Add(int.Parse(input));
+                input = Console.ReadLine();
+            }
+            foreach (var number in list.Where(x => x < 1000))
+            {
+                foreach (var secondNumber in list.Where(x => x >=1000))
+                {
+                    if (number + secondNumber == 2020)
+                    {
+                        Console.WriteLine(number * secondNumber);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
