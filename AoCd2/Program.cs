f78bb8c30f78a44ff837d9bace48bf4eed045a5b
Delyan Dimitrov <delyandimitrov99@gmail.com>
2020-12-05 23:28:07 +0200

    Did part2 of Day 1

---
+++ AoCd2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoCd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var list = new List<int>();
+            while (input != "STOP")
+            {
+                list.Add(int.Parse(input));
+                input = Console.ReadLine();
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    for (int k = 0; k < list.Count; k++)
+                    {
+                        if (list[i] + list[j] + list[k] == 2020)
+                        {
+                            Console.WriteLine(list[i] * list[j] * list[k]);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
